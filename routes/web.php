0229ca3c271dbd432f327a619909c99d181a181c
Dávid Držík <72087861+daviddrzik@users.noreply.github.com>
2025-02-24 14:22:34 +0100

    First routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    echo "Hello World!";
+});
+
+Route::get('/test', [TestController::class, 'testAction']);
+
+
+
